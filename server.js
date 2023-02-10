3274d9e92925a124a74ff414493a3fded6a65c4f
Francis M <117141709+mfrancislie@users.noreply.github.com>
2023-02-10 00:09:57 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import path from 'path';
+
 
 dotenv.config();
 const app = express();
@@ -134,9 +134,7 @@ app.get('/api/seed', async (req, res) => {
   res.send({ message: 'Seeded Successfully' });
 });
 
-const dirname = path.resolve();
-app.use('/', express.static(dirname + '/build'));
-app.get('/', (req, res) => res.sendFile(dirname + '/build/index.html'));
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`served at http://localhost:5000`));
